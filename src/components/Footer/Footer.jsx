9fc05e949ba269caf1cc15c3617e2b4ee4c09201
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2024-10-25 00:25:30 +0600

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,7 +22,7 @@ export default function Footer() {
             <nav>
                 <h6 className="footer-title">Company</h6>
                 <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Email</a>
                 <a className="link link-hover">Jobs</a>
                 <a className="link link-hover">Press kit</a>
             </nav>
