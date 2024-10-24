263acea187043a8d87d9f5f59109f83af43aa9c7
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2024-10-25 00:25:12 +0600

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
             </aside>
             <nav>
                 <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Modles</a>
                 <a className="link link-hover">Design</a>
                 <a className="link link-hover">Marketing</a>
                 <a className="link link-hover">Advertisement</a>
